fdcfefec39983b04e64c5a82f5812c869fe4423b
Ivan Blazevic <ivanblazevic17@gmail.com>
2016-07-19 15:57:16 +0200

    build process and deploy automated

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,18 @@ var gulp     = require('gulp'),
     watch    = require('gulp-watch'),
     cleanCSS = require('gulp-clean-css'),
     rename   = require('gulp-rename'),
+    s3       = require('gulp-s3'),
+    shell    = require('gulp-shell'),
+    fs       = require('fs'),
     del      = require('del');
 
-gulp.task('delete-min', function() {
+gulp.task('delete-build', function() {
   return del([
-    'build/styles/*.min.css',
-    'build/css/'
+    'build/'
   ]);
 });
 
-gulp.task('minify-css', ['delete-min'], function() {
+gulp.task('build', ['metalsmith'], function() {
   return gulp.src('src/css/*.css')
     .pipe(cleanCSS())
     .pipe(rename({
@@ -20,10 +22,17 @@ gulp.task('minify-css', ['delete-min'], function() {
     .pipe(gulp.dest('build/styles/'));
 });
 
-gulp.task('generate-css', ['delete-min', 'minify-css']);
+gulp.task('metalsmith', ['delete-build'], shell.task([
+  'node build.js --exit'
+]))
 
-gulp.task('watch', function() {
-  watch('src/css/*.css', function() {
-    gulp.start('generate-css');
-  })
+gulp.task('publish', ['build'], function(){
+  var aws = JSON.parse(fs.readFileSync('./.aws.json'));
+
+  var options = {
+    headers: {'Cache-Control': 'max-age=315360000, no-transform, public'}
+  };
+
+  return gulp.src('./build/**')
+    .pipe(s3(aws, options));
 });
